fb07a502543621211ecdd443e3c856b58a236033
KayliedeBlois <kayliedeblois@gmail.com>
2022-08-23 21:46:25 -0400

    Add exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+#total revenue for entire company
+@stores_sum = Store.sum(:annual_revenue)
+
+#average revenue 
+average_revenue = @stores_sum / Store.count
+puts average_revenue
+
+#stores with $1M or more in annual sales
+@million_dollar_stores = Store.where(annual_revenue: ..1000000).count
+puts @million_dollar_stores
